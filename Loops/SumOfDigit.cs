17aeafa008385a62d23c8a83aab436dfe6fd0c53
Megha013 <meghaawasthi013@gmail.com>
2024-10-18 07:58:57 +0530

    commit

---
+++ Loops/SumOfDigit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class SumOfDigit
+    {
+        static void Main(string[] args)
+        {
+            //Display the sum of digits of number
+            int num = 1234;
+            int sum = 0;
+            for(int i=num; i>0; i=i/10)
+            {
+                int digit = i % 10;
+                sum += digit;
+            }
+            Console.WriteLine("Sum is : "+sum);
+            Console.ReadLine();
+        }
+    }
+}
