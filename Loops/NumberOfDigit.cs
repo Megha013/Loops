44d816aee4b570ac966650e76d04ab01bebbac48
Megha013 <meghaawasthi013@gmail.com>
2024-10-18 08:10:22 +0530

    Add new file

---
+++ Loops/NumberOfDigit.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Loops
+//{
+//    internal class NumberOfDigit
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Count the number of digits in any number. 
+//            int num = 12398;
+//            int count = 0;
+//            for (int i = num; i > 0; i /= 10)
+//            {
+//                count++;
+//            }
+//            Console.WriteLine("Total count : "+count);
+//        }
+//    }
+//}
--- Loops/SumOfDigit.cs
+++ Loops/SumOfDigit.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Loops
-{
-    internal class SumOfDigit
-    {
-        static void Main(string[] args)
-        {
-            //Display the sum of digits of number
-            int num = 1234;
-            int sum = 0;
-            for(int i=num; i>0; i=i/10)
-            {
-                int digit = i % 10;
-                sum += digit;
-            }
-            Console.WriteLine("Sum is : "+sum);
-            Console.ReadLine();
-        }
-    }
-}
+//namespace Loops
+//{
+//    internal class SumOfDigit
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Display the sum of digits of number
+//            int num = 1234;
+//            int sum = 0;
+//            for(int i=num; i>0; i=i/10)
+//            {
+//                int digit = i % 10;
+//                sum += digit;
+//            }
+//            Console.WriteLine("Sum is : "+sum);
+//            Console.ReadLine();
+//        }
+//    }
+//}
